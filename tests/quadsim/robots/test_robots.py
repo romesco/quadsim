b58fde058a9e3906f732878993fc9d0bbc1267ce
romesc <rosario@cs.washington.edu>
2021-05-01 19:29:32 -0700

    skip incomplete test

--- tests/quadsim/robots/test_robots.py
+++ tests/quadsim/robots/test_robots.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pybullet
 import pybullet_data
+import pytest
 from pybullet_utils import bullet_client
 from quadsim.robots.a1 import A1
 from quadsim.simulator import SimulatorConf
@@ -29,69 +30,51 @@ def setup():
     return p, sim_conf
 
 
-# @pytest.mark.parametrize(
-#    "on_rack, hard_reset",
-#    [
-#        pytest.param(
-#            True,
-#            True,
-#            id="0",
-#        ),
-#        pytest.param(
-#            True,
-#            False,
-#            id="1",
-#        ),
-#        pytest.param(
-#            False,
-#            True,
-#            id="2",
-#        ),
-#        pytest.param(
-#            False,
-#            False,
-#            id="3",
-#        ),
-#    ]
-# )
-# def test_reset(on_rack, hard_reset):
-#    pybullet_client, sim_conf = setup()
-#    robot = A1(pybullet_client=pybullet_client, sim_conf=sim_conf)
-#    if hard_reset:
-#        robot._pybullet_client.resetSimulation()
-#        robot._pybullet_client.setAdditionalSearchPath(pybullet_data.getDataPath())
-#        robot._pybullet_client.loadURDF("plane.urdf")
-#    robot.reset(hard_reset=hard_reset)
-#    if on_rack:
-#        np.testing.assert_allclose(
-#            robot.base_position, sim_conf.init_rack_position, atol=0.01
-#        )
-#    else:
-#        np.testing.assert_allclose(
-#            robot.base_position, sim_conf.init_position, atol=0.1
-#        )
-#    np.testing.assert_allclose(
-#        robot.base_orientation_rpy, [0.0, 0.0, 0.0], atol=0.03
-#    )
-#    np.testing.assert_allclose(
-#        robot.base_orientation_quat, [0.0, 0.0, 0.0, 1.0], atol=0.01
-#    )
-#    np.testing.assert_allclose(robot.base_velocity, [0.0, 0.0, 0.0], atol=0.01)
-#    np.testing.assert_allclose(robot.base_rpy_rate, [0.0, 0.0, 0.0], atol=0.01)
-#    if on_rack:
-#        np.testing.assert_allclose(
-#            robot.motor_angles, robot._motor_group._init_positions, atol=0.01
-#        )
-#    else:
-#        np.testing.assert_allclose(
-#            robot.motor_angles, robot._motor_group._init_positions, atol=0.1
-#        )
-#    np.testing.assert_allclose(robot.motor_velocities, np.zeros(12), atol=0.01)
-#    np.testing.assert_allclose(
-#        robot.control_timestep,
-#        sim_conf.timestep * sim_conf.action_repeat,
-#    )
-#    np.testing.assert_allclose(robot.time_since_reset, 0.0)
+@pytest.mark.skip(reason="TODO: robot reset test needs implementation")
+@pytest.mark.parametrize(
+    "on_rack, hard_reset",
+    [
+        pytest.param(True, True, id="0",),
+        pytest.param(True, False, id="1",),
+        pytest.param(False, True, id="2",),
+        pytest.param(False, False, id="3",),
+    ],
+)
+def test_reset(on_rack, hard_reset):
+    pybullet_client, sim_conf = setup()
+    robot = A1(pybullet_client=pybullet_client, sim_conf=sim_conf)
+    if hard_reset:
+        robot._pybullet_client.resetSimulation()
+        robot._pybullet_client.setAdditionalSearchPath(pybullet_data.getDataPath())
+        robot._pybullet_client.loadURDF("plane.urdf")
+    robot.reset(hard_reset=hard_reset)
+    if on_rack:
+        np.testing.assert_allclose(
+            robot.base_position, sim_conf.init_rack_position, atol=0.01
+        )
+    else:
+        np.testing.assert_allclose(
+            robot.base_position, sim_conf.init_position, atol=0.1
+        )
+    np.testing.assert_allclose(robot.base_orientation_rpy, [0.0, 0.0, 0.0], atol=0.03)
+    np.testing.assert_allclose(
+        robot.base_orientation_quat, [0.0, 0.0, 0.0, 1.0], atol=0.01
+    )
+    np.testing.assert_allclose(robot.base_velocity, [0.0, 0.0, 0.0], atol=0.01)
+    np.testing.assert_allclose(robot.base_rpy_rate, [0.0, 0.0, 0.0], atol=0.01)
+    if on_rack:
+        np.testing.assert_allclose(
+            robot.motor_angles, robot._motor_group._init_positions, atol=0.01
+        )
+    else:
+        np.testing.assert_allclose(
+            robot.motor_angles, robot._motor_group._init_positions, atol=0.1
+        )
+    np.testing.assert_allclose(robot.motor_velocities, np.zeros(12), atol=0.01)
+    np.testing.assert_allclose(
+        robot.control_timestep, sim_conf.timestep * sim_conf.action_repeat,
+    )
+    np.testing.assert_allclose(robot.time_since_reset, 0.0)
 
 
 def test_step():
